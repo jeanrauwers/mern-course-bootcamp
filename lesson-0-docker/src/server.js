41166a5b8a4d90d5ac46cc9c16b54198919e43fe
Jean Rauwers <jeanrauwers@gmail.com>
2020-05-13 20:12:33 +0100

    Formatting

--- lesson-0-docker/src/server.js
+++ lesson-0-docker/src/server.js
@@ -3,14 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = 8080;
 
-
-
 const connection = "mongodb://mongo:27017/mongo-test";
 
 app.get("/", (req, res) => {
     res.send("Hello from Node.js app \n");
 });
-
 
 try {
     mongoose.connect(connection);
