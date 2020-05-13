81f8477198174f77c5c108e7d37133c89d78ba2e
Jean Rauwers <jeanrauwers@gmail.com>
2020-05-13 22:04:38 +0100

    Finish first user

--- lesson-0-docker/src/server.js
+++ lesson-0-docker/src/server.js
@@ -1,16 +1,39 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors')
+const RegisterController = require('./controllers/RegisterController')
+const app = express();
 const PORT = 8080;
 
-const connection = "mongodb://mongo:27017/mongo-test";
+
+
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
 app.get("/", (req, res) => {
     res.send("Hello from Node.js app \n");
 });
 
+app.get("/register", (req, res) => {
+    res.send("Welcome to Register \n");
+});
+
+app.post("/register",RegisterController.store)
+
 try {
-    mongoose.connect(connection);
+    mongoose.connect(
+        process.env.MONGO_DB_SECRET,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    );
+    console.log('MongoDb connected successfully!')
+
 
 } catch (error) {
     console.log(error)
