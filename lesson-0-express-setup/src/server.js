cd95029a64e92618153457f383789418615faaf6
Jean Rauwers <jeanrauwers@gmail.com>
2020-05-17 17:06:16 +0100

    Update server port

--- lesson-0-express-setup/src/server.js
+++ lesson-0-express-setup/src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const UserController = require('./controllers/UserController')
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
