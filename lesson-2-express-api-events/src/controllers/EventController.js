7cc5ae0a1626d9490c3869dc7f42b3db33003178
Jean Rauwers <jeanrauwers@gmail.com>
2020-05-20 15:33:54 +0100

    Update folder name

--- lesson-2-express-api-events/src/controllers/EventController.js
+++ lesson-2-express-api-events/src/controllers/EventController.js
@@ -25,33 +25,6 @@ module.exports = {
         return res.json(event);
     },
 
-    async getEventById(req, res) {
-        const { eventId } = req.params;
-        try {
-            const event = await Event.findById(eventId)
-
-            if (event) {
-                return res.json(event)
-            }
-        } catch (error) {
-            return res.status(400).json({ message: 'EventId does not exist!' })
-        }
-    },
-    async getAllEvents(req, res) {
-        const { sport } = req.params;
-        const query = { sport } || {}
-
-        try {
-            const events = await Event.find(query)
-
-            if (events) {
-                return res.json(events)
-            }
-        } catch (error) {
-            return res.status(400).json({ message: 'We do have any events yet' })
-        }
-    },
-
     async delete(req, res) {
         const { eventId } = req.params;
         try {
