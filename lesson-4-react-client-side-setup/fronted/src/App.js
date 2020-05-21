aab9acba0420922cd361419c30b882495984ca82
Jean Rauwers <jeanrauwers@gmail.com>
2020-05-21 19:51:36 +0100

    lesson-4 first commit

---
+++ lesson-4-react-client-side-setup/fronted/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Learning the MERN stack.
+    </div>
+  );
+}
+
+export default App;
