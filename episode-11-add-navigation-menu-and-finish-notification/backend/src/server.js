c7316ff9f87bae3c2bc48b02be9864f10547fa20
Jean Rauwers <jeanrauwers@gmail.com>
2020-06-09 14:48:31 +0100

    Add episode 11 first commit

---
+++ episode-11-add-navigation-menu-and-finish-notification/backend/src/server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+const PORT = process.env.PORT || 8000
+
+
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+try {
+	mongoose.connect(process.env.MONGO_DB_SECRET, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	console.log('MongoDb connected successfully!')
+} catch (error) {
+	console.log(error)
+}
+
+const connectUsers = {}
+
+io.on('connection', socket => {
+	const { user } = socket.handshake.query
+
+	connectUsers[user] = socket.id
+})
+
+//app.use()
+app.use((req, res, next) => {
+	req.io = io
+	req.connectUsers = connectUsers
+	return next()
+})
+app.use(cors())
+app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'files')))
+app.use(routes)
+
+server.listen(PORT, () => {
+	console.log(`Listening on ${PORT}`)
+})
---
+++ episode-11-add-navigation-menu-and-finish-notification/fronted/src/pages/Dashboard/index.js
@@ -0,0 +1,171 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import api from '../../services/api';
+import moment from 'moment';
+import { Button, ButtonGroup, Alert } from 'reactstrap';
+import socketio from 'socket.io-client';
+import './dashboard.css'
+//Dashboard will show all the events 
+export default function Dashboard({ history }) {
+    const [events, setEvents] = useState([]);
+    const user = localStorage.getItem('user');
+    const user_id = localStorage.getItem('user_id');
+
+    const [rSelected, setRSelected] = useState(null);
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false)
+    const [messageHandler, setMessageHandler] = useState('');
+    const [eventsRequest, setEventsRequest] = useState([])
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const socket = useMemo(
+        () =>
+            socketio('http://localhost:8000/', { query: { user: user_id } }),
+        [user_id]
+    );
+
+    useEffect(() => {
+        socket.on('registration_request', data => setEventsRequest([...eventsRequest, data]));
+    }, [eventsRequest, socket])
+
+    const filterHandler = (query) => {
+        setRSelected(query)
+        getEvents(query)
+    }
+
+    const myEventsHandler = async () => {
+        try {
+            setRSelected('myevents')
+            const response = await api.get('/user/events', { headers: { user } })
+            setEvents(response.data.events)
+        } catch (error) {
+            history.push('/login');
+
+        }
+
+    }
+
+    const getEvents = async (filter) => {
+        try {
+            const url = filter ? `/dashboard/${filter}` : '/dashboard';
+            const response = await api.get(url, { headers: { user } })
+
+            setEvents(response.data.events)
+        } catch (error) {
+            history.push('/login');
+        }
+
+    };
+
+    const deleteEventHandler = async (eventId) => {
+        try {
+            await api.delete(`/event/${eventId}`, { headers: { user: user } });
+            setSuccess(true)
+            setMessageHandler('The event was deleted successfully!')
+            setTimeout(() => {
+                setSuccess(false)
+                filterHandler(null)
+                setMessageHandler('')
+            }, 2500)
+
+        } catch (error) {
+            setError(true)
+            setMessageHandler('Error when deleting event!')
+            setTimeout(() => {
+                setError(false)
+                setMessageHandler('')
+            }, 2000)
+        }
+    }
+
+    const logoutHandler = () => {
+        localStorage.removeItem('user')
+        localStorage.removeItem('user_id')
+        history.push('/login');
+    }
+
+    const registrationRequestHandler = async (event) => {
+        try {
+            await api.post(`/registration/${event.id}`, {}, { headers: { user } })
+
+            setSuccess(true)
+            setMessageHandler(`The request for the event ${event.title} was successfully!`)
+            setTimeout(() => {
+                setSuccess(false)
+                filterHandler(null)
+                setMessageHandler('')
+            }, 2500)
+
+        } catch (error) {
+            setError(true)
+            setMessageHandler(`The request for the event ${event.title} wasn't successfully!`)
+            setTimeout(() => {
+                setError(false)
+                setMessageHandler('')
+            }, 2000)
+        }
+    }
+
+    return (
+        <>
+            <ul className="notifications">
+                {eventsRequest.map(request => {
+                    console.log(request)
+                    return (
+                        <li key={request.id}>
+                            <div>
+                                <strong>{request.user.email} </strong> is requesting to register to your Event <strong>{request.event.title}</strong>
+                            </div>
+                            <ButtonGroup>
+
+                                <Button color="secondary" onClick={() => { }}>Accept</Button>
+                                <Button color="danger" onClick={() => { }}>Cancel</Button>
+                            </ButtonGroup>
+                        </li>
+                    )
+                })}
+
+            </ul>
+            <div className="filter-panel">
+                <ButtonGroup>
+                    <Button color="primary" onClick={() => filterHandler(null)} active={rSelected === null}>All Sports</Button>
+                    <Button color="primary" onClick={myEventsHandler} active={rSelected === 'myevents'}>My Events</Button>
+                    <Button color="primary" onClick={() => filterHandler("running")} active={rSelected === 'running'}>Running</Button>
+                    <Button color="primary" onClick={() => filterHandler("cycling")} active={rSelected === 'cycling'}>Cycling</Button>
+                    <Button color="primary" onClick={() => filterHandler('swimming')} active={rSelected === 'swimming'}>Swimming</Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button color="secondary" onClick={() => history.push('events')}>Events</Button>
+                    <Button color="danger" onClick={logoutHandler}>Logout</Button>
+                </ButtonGroup>
+            </div>
+            <ul className="events-list">
+                {events.map(event => (
+                    <li key={event._id}>
+                        <header style={{ backgroundImage: `url(${event.thumbnail_url})` }}>
+                            {event.user === user_id ? <div><Button color="danger" size="sm" onClick={() => deleteEventHandler(event._id)}>Delete</Button></div> : ""}
+
+                        </header>
+                        <strong>{event.title}</strong>
+                        <span>Event Date: {moment(event.date).format('l')}</span>
+                        <span>Event Price: {parseFloat(event.price).toFixed(2)}</span>
+                        <span>Event Description: {event.description}</span>
+                        <Button color="primary" onClick={() => registrationRequestHandler(event)}>Registration Request</Button>
+                    </li>
+                ))}
+            </ul>
+            {
+                error ? (
+                    <Alert className="event-validation" color="danger"> {messageHandler} </Alert>
+                ) : ""
+            }
+            {
+                success ? (
+                    <Alert className="event-validation" color="success"> {messageHandler}</Alert>
+                ) : ""
+            }
+        </>
+    )
+}
